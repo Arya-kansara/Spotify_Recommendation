5d01ec340e1c5fef2169c71ca238a4bca71e9120
Arya-kansara <kansaraarya00@gmail.com>
2026-01-22 22:48:08 +0530

    Refactor Flask app to support song prediction via form
    Updated home route to handle POST requests and render index.html with song prediction. Changed app run configuration to use port 10000.

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from spotify_recommendation import next_song_prediction
 
+# Initialize Flask (Defaults: template_folder='templates', static_folder='static')
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Spotify ML Recommendation API is running"
+    next_song = None
+    if request.method == "POST":
+        # Get song name from form
+        song_name = request.form.get("track_name")
+        # Get prediction
+        next_song = next_song_prediction(song_name)
+    
+    return render_template("index.html", next_song=next_song)
 
 @app.route("/predict", methods=["POST"])
-def predict():
+def predict_api():
     data = request.json
     song = data.get("song")
     result = next_song_prediction(song)
-
     if result is None:
         return jsonify({"error": "Song not found"})
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    # Port 10000 is required for Render
+    app.run(host="0.0.0.0", port=10000)
